dd2d6f8a70c10921209002acb130b4f118d8085c
viper404 <viper404@163.com>
2016-07-01 11:34:11 +0800

    Update util.js

--- 2015_spring/task0002/util.js
+++ 2015_spring/task0002/util.js
@@ -89,3 +89,13 @@ function simpleTrim(str) {
 var str = '   hi!  ';
 str = simpleTrim(str);
 console.log(str);   // 'hi!'
+// 很多同学肯定对于上面的代码看不下去，接下来，我们真正实现一个trim
+// 对字符串头尾进行空格字符的去除、包括全角半角空格、Tab等，返回一个字符串
+// 尝试使用一行简洁的正则表达式完成该题目
+function trim(str) {
+    // your implement
+}
+// 使用示例
+var str = '   hi!  ';
+str = trim(str);
+console.log(str);   // 'hi!'
